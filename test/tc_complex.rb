a5fd433002c88e5d03c5bc8b560b9ee4631e0ea4
Sabato Ceruso <alu0100764666@ull.edu.es>
2014-10-17 12:19:21 +0100

    Arreglado string dentro del test

--- test/tc_complex.rb
+++ test/tc_complex.rb
@@ -10,13 +10,13 @@ class TestComplejo < Test::Unit::TestCase
     # nothing
   end
   def test_simple
-    assert_equal("(0,0)", @origen.to_s)
-    assert_equal("(5,5)", (@unidad*5).to_s)
+    assert_equal("(0,0i)", @origen.to_s)
+    assert_equal("(5,5i)", (@unidad*5).to_s)
   end
   def test_type_check
     assert_raise(RuntimeError) {Complejo.new('1','1')}
   end
   def test_failure
-    assert_equal("(5,5)", (@origen * 5).to_s, "Producto escalar")
+    assert_equal("(5,5i)", (@origen * 5).to_s, "Producto escalar")
   end
 end
