304111c5aeed735366bf0cab7ea6575804b7a495
Javier Martín Hernández <alu0100777758@ull.edu.es>
2014-10-17 12:18:21 +0100

    Añadido class Complejo suma y resta

---
+++ lib/JavierComplex.rb
@@ -0,0 +1,26 @@
+class Complejo
+  attr_reader :a, :b
+  attr_writer :a, :b
+  def initialize(a, b)
+    raise unless a.is_a?(Numeric)
+    raise unless b.is_a?(Numeric)
+    @a, @b = a, b
+  end
+  def to_s
+	salida="(#{@a}"
+	if @b<0
+		salida=salida+"#{@b}i)"
+	elsif @b>0
+		salida=salida+"+#{@b}i)"
+	end
+	salida
+  end
+  def + (other)
+    Complejo.new(@a + other.a, @b + other.b)
+  end
+  def - (other)
+    Complejo.new(@a-other.a,@b-other-b)
+  end
+end
+
+
